792fa29a35fd51c637fa0324b96f75644e51aa2b
pemborongbangunansemarang <bangunsemar@gmail.com>
2023-01-04 11:16:48 +0700

    Signed-off-by: pemborongbangunansemarang <bangunsemar@gmail.com>

---
+++ wp-content/plugins/powerpack-elements/assets/js/editor.js
@@ -0,0 +1,12 @@
+/*
+( function( $ ) {
+
+	$( window ).on( 'madxartwork:init', function() {
+        madxartwork.hooks.addAction( 'panel/open_editor/widget/', function( panel, model, view ) {
+		madxartwork.hooks.addAction('panel/open_editor/widget/pp-woo-products', function (panel, model, view) {
+			console.log(panel);
+		});
+        } );
+	} );
+} )( jQuery );
+*/
